c3bd9a4e926f84fc0ee1949c62c167801c0b13b3
Harenson Henao <harenson@gmail.com>
2016-02-19 13:34:53 -0500

    Add tests

---
+++ test/config_SUITE.erl
@@ -0,0 +1,36 @@
+-module(config_SUITE).
+
+%% CT
+-export([
+  all/0,
+  init_per_suite/1,
+  end_per_suite/1
+]).
+
+%% Test Cases
+-export([check_overrun_handler/1]).
+
+-type config() :: [{atom(), term()}].
+
+%%%=============================================================================
+%%% Common test
+%%%=============================================================================
+
+-spec all() -> [atom()].
+all() -> [check_overrun_handler].
+
+-spec init_per_suite(config()) -> config().
+init_per_suite(Config) ->
+  ok = test_utils:start_apps(),
+  Config.
+
+-spec end_per_suite(config()) -> config().
+end_per_suite(Config) ->
+  Config.
+
+%%%=============================================================================
+%%% Exported Tests Functions
+%%%=============================================================================
+
+check_overrun_handler(_Config) ->
+  sumo:call(sumo_test_people_mysql, takes_too_long, []).
